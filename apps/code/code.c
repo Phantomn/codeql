9e2dbee542c8138fc2b46ea791286b308a0cd3c8
Phantomn <tmdvyr123@gmail.com>
2020-12-11 14:26:47 +0900

    Add apps/databases/results/workspace

---
+++ apps/code/code.c
@@ -0,0 +1,12 @@
+int main(int argc, char* argv[]){
+    int size1=5;
+    int size2= size1 + 5;
+
+    char *dst = malloc(size1);
+    char *src = malloc(size2);
+
+    if(strcmp(argv, "change") == 0)
+    size1 = 15;
+
+    memcpy(dst, src, size1);
+}
